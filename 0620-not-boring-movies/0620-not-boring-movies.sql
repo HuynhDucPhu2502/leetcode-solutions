c7c5da2f1bb504024319b2317e056c940bb472f6
Huỳnh Đức Phú <159509986+HuynhDucPhu2502@users.noreply.github.com>
2026-07-11 19:41:11 +0700

    Time: 183 ms (77.61%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT c.id, c.movie, c.description, c.rating
+FROM Cinema c
+WHERE c.id % 2 != 0 AND c.description != 'boring'
+ORDER BY c.rating DESC
