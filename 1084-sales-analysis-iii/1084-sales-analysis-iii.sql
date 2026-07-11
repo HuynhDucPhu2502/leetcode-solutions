64e769baf2f4990791cb553ebb8952abff56ae73
Huỳnh Đức Phú <159509986+HuynhDucPhu2502@users.noreply.github.com>
2026-07-11 22:18:45 +0700

    Time: 965 ms (43.44%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s
+    ON p.product_id = s.product_id
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Sales s2
+    WHERE s2.product_id = p.product_id AND (s2.sale_date < '2019-01-01' OR s2.sale_date > '2019-03-31')
+)
